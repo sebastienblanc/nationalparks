f114b66047b9ea0d4017f71f4647ec8fbd9f6ed0
sebastienblanc <scm.blanc@gmail.com>
2020-10-28 21:59:33 +0100

    other fixes

--- src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
+++ src/main/java/com/openshift/evg/roadshow/parks/rest/BackendController.java
@@ -17,6 +17,6 @@ public class BackendController{
 
     @RequestMapping(method = RequestMethod.GET, value = "/", produces = "application/json")
     public Backend get() {
-       return new Backend("nationalparks","Hello TDC ! National Parks", new Coordinates("47.039304", "14.505178"), 4);
+       return new Backend("nationalparks","Hello TDC , in the Red Hat Room ! National Parks", new Coordinates("47.039304", "14.505178"), 4);
     }
 }
